a46a5b5e8c88ade065d708d4920756ae3ebf95b6
adicherla venkata sai <adicherlavenkatasai5@gmail.com>
2020-08-27 21:01:35 +0530

    Add files via upload

---
+++ a18/code/DATABASE/admincloud.sql
@@ -0,0 +1,82 @@
+/*
+SQLyog Community v8.71 
+MySQL - 5.5.30 : Database - admincloud
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`admincloud` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `admincloud`;
+
+/*Table structure for table `admindata` */
+
+DROP TABLE IF EXISTS `admindata`;
+
+CREATE TABLE `admindata` (
+  `ID` int(11) NOT NULL,
+  `uname` varchar(255) DEFAULT NULL,
+  `Filename` varchar(255) DEFAULT NULL,
+  `Filepath` varchar(255) DEFAULT NULL,
+  `Date` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `adminlogin` */
+
+DROP TABLE IF EXISTS `adminlogin`;
+
+CREATE TABLE `adminlogin` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uname` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `confirmpassword` varchar(45) NOT NULL,
+  `Dob` varchar(45) NOT NULL,
+  `Email` varchar(45) NOT NULL,
+  `mobilenumber` varchar(45) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `contact` */
+
+DROP TABLE IF EXISTS `contact`;
+
+CREATE TABLE `contact` (
+  `ID` int(11) NOT NULL,
+  `contactUser` varchar(255) DEFAULT NULL,
+  `contactsubject` varchar(255) DEFAULT NULL,
+  `contactmail` varchar(255) DEFAULT NULL,
+  `contactweb` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `ID` int(11) NOT NULL,
+  `uname` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `confirmpassword` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `Dob` varchar(255) DEFAULT NULL,
+  `mobilenumber` varchar(255) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `securityquestion` varchar(255) DEFAULT NULL,
+  `securityanswer` varchar(255) DEFAULT NULL,
+  `securitykey` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ a18/code/DATABASE/transactions.sql
@@ -0,0 +1,146 @@
+/*
+SQLyog Community v8.71 
+MySQL - 5.5.30 : Database - transactions
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`transactions` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `transactions`;
+
+/*Table structure for table `admindata` */
+
+DROP TABLE IF EXISTS `admindata`;
+
+CREATE TABLE `admindata` (
+  `ID` int(11) NOT NULL,
+  `uname` varchar(255) DEFAULT NULL,
+  `Filename` varchar(255) DEFAULT NULL,
+  `Filepath` varchar(255) DEFAULT NULL,
+  `Date` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `contact` */
+
+DROP TABLE IF EXISTS `contact`;
+
+CREATE TABLE `contact` (
+  `ID` int(11) NOT NULL,
+  `contactUser` varchar(255) DEFAULT NULL,
+  `contactsubject` varchar(255) DEFAULT NULL,
+  `contactmail` varchar(255) DEFAULT NULL,
+  `contactweb` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `home_values` */
+
+DROP TABLE IF EXISTS `home_values`;
+
+CREATE TABLE `home_values` (
+  `values` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `homedatabase` */
+
+DROP TABLE IF EXISTS `homedatabase`;
+
+CREATE TABLE `homedatabase` (
+  `Id` int(11) NOT NULL,
+  `UserName` varchar(255) DEFAULT NULL,
+  `FileName` varchar(255) DEFAULT NULL,
+  `FilePath` varchar(255) DEFAULT NULL,
+  `Date` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `life_values` */
+
+DROP TABLE IF EXISTS `life_values`;
+
+CREATE TABLE `life_values` (
+  `values` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `lifedatabase` */
+
+DROP TABLE IF EXISTS `lifedatabase`;
+
+CREATE TABLE `lifedatabase` (
+  `Id` int(11) NOT NULL,
+  `UserName` varchar(255) DEFAULT NULL,
+  `FileName` varchar(255) DEFAULT NULL,
+  `Date` varchar(255) DEFAULT NULL,
+  `PathLocation` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `ID` int(11) NOT NULL,
+  `uname` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `confirmpassword` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `Dob` varchar(255) DEFAULT NULL,
+  `mobilenumber` varchar(255) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `securityquestion` varchar(255) DEFAULT NULL,
+  `securityanswer` varchar(255) DEFAULT NULL,
+  `securitykey` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `medical_values` */
+
+DROP TABLE IF EXISTS `medical_values`;
+
+CREATE TABLE `medical_values` (
+  `values` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `medicaldatabase` */
+
+DROP TABLE IF EXISTS `medicaldatabase`;
+
+CREATE TABLE `medicaldatabase` (
+  `Id` int(11) NOT NULL,
+  `UserName` varchar(255) DEFAULT NULL,
+  `FileName` varchar(255) DEFAULT NULL,
+  `PathLocation` varchar(255) DEFAULT NULL,
+  `Date` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `newtable` */
+
+DROP TABLE IF EXISTS `newtable`;
+
+CREATE TABLE `newtable` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uname` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `confirmpassword` varchar(45) NOT NULL,
+  `Email` varchar(45) NOT NULL,
+  `Dob` varchar(45) NOT NULL,
+  `mobilenumber` varchar(45) NOT NULL,
+  `Address` varchar(45) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
